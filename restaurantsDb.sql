39d5b8161be225a7cd46e65f9b054ef247de7786
jkroks <47325028+jkroks@users.noreply.github.com>
2019-02-09 18:23:18 +0900

    Update restaurantsDb.sql

--- restaurantsDb.sql
+++ restaurantsDb.sql
@@ -1,2 +1,17 @@
 create database restaurant;
 use restaurant;
+
+
+CREATE TABLE restaurant (
+        ID VARCHAR(100) NOT NULL,
+        RESTAURANT VARCHAR(500) NOT NULL,
+        RESTTYPE VARCHAR(500) NOT NULL,
+        WEBPAGE VARCHAR(5000),
+        IMAGE VARCHAR(5000),
+        AREA  VARCHAR(500),
+        PREFACTURE VARCHAR(500),
+        BUDGET INT,
+        LATITUDE DOUBLE,
+        LONGITUDE DOUBLE,
+        PRIMARY KEY (ID)
+    );
